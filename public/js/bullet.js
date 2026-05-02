6fd903f35bd89e52a780a2494a8a3b1efed56625
yeziyuhai <112500386+yeziyuhai@users.noreply.github.com>
2026-05-02 23:06:53 +0800

    Update bullet.js

--- public/js/bullet.js
+++ public/js/bullet.js
@@ -1,1 +1,20 @@
 
+// public/js/bullet.js - 子弹类
+class Bullet {
+    constructor(x, y, direction, owner, damage, knockback) {
+        this.x = x;
+        this.y = y;
+        this.width = 8;
+        this.height = 5;
+        this.direction = direction;
+        this.owner = owner;
+        this.damage = damage;
+        this.knockback = knockback;
+        this.speed = 10;
+    }
+    
+    update() {
+        this.x += this.speed * this.direction;
+        return this.x > -50 && this.x < 800;
+    }
+}
